e50a3b70304d3eb74513db3a132f8036253ad360
Scootifer <Windmill3341!>
2021-09-05 11:24:36 -0400

    Initial commit

---
+++ src/main/java/Solution1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter a noun: ");
+        String noun = scan.nextLine();
+
+        System.out.print("\nPlease enter a verb: ");
+        String verb = scan.nextLine();
+
+        System.out.print("\nPlease enter an adjective: ");
+        String adjective = scan.nextLine();
+
+        System.out.print("\nPlease enter an adverb: ");
+        String adverb = scan.nextLine();
+
+        System.out.println("Did you " + verb + " the " + adjective + " " + noun + " " + adverb + "?");
+    }
+}
